033a7b9f8272b5380440a0c5a36fc85da9caf9f2
Sebas21EC <sebastiantabango@gmail.com>
2022-04-27 19:32:27 -0500

    Version Final

---
+++ CuadradoMagico_Form.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace CuadradoMagico_CSharp
+{
+    partial class CuadradoMagico_Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnResolver = new System.Windows.Forms.Button();
+            this.tmrLlenar = new System.Windows.Forms.Timer(this.components);
+            this.trkbrIntervalo = new System.Windows.Forms.TrackBar();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trkbrIntervalo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtN
+            // 
+            this.txtN.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtN.Location = new System.Drawing.Point(134, 34);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(30, 29);
+            this.txtN.TabIndex = 0;
+            this.txtN.Text = "5";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGenerar.Location = new System.Drawing.Point(184, 34);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(91, 29);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(675, 522);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(50, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tamaño";
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnResolver.Location = new System.Drawing.Point(499, 12);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(91, 29);
+            this.btnResolver.TabIndex = 5;
+            this.btnResolver.Text = "Resolver";
+            this.btnResolver.UseVisualStyleBackColor = true;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // tmrLlenar
+            // 
+            this.tmrLlenar.Interval = 1000;
+            this.tmrLlenar.Tick += new System.EventHandler(this.tmrLlenar_Tick);
+            // 
+            // trkbrIntervalo
+            // 
+            this.trkbrIntervalo.Location = new System.Drawing.Point(596, 14);
+            this.trkbrIntervalo.Minimum = 1;
+            this.trkbrIntervalo.Name = "trkbrIntervalo";
+            this.trkbrIntervalo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trkbrIntervalo.Size = new System.Drawing.Size(104, 45);
+            this.trkbrIntervalo.TabIndex = 6;
+            this.trkbrIntervalo.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trkbrIntervalo.Value = 10;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnVerificar.Location = new System.Drawing.Point(357, 34);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 32);
+            this.btnVerificar.TabIndex = 7;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // CuadradoMagico_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 602);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.trkbrIntervalo);
+            this.Controls.Add(this.btnResolver);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtN);
+            this.MaximizeBox = false;
+            this.Name = "CuadradoMagico_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cuadrado Mágico";
+            ((System.ComponentModel.ISupportInitialize)(this.trkbrIntervalo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnResolver;
+        private System.Windows.Forms.Timer tmrLlenar;
+        private System.Windows.Forms.TrackBar trkbrIntervalo;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ CuadradoMagico_Form.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuadradoMagico_CSharp
+{
+    public partial class CuadradoMagico_Form : Form
+    {
+        public CuadradoMagico_Form()
+        {
+            InitializeComponent();
+            matriz_textbox = new TextBox[15, 15];
+            fuente = new Font("Arial", 11);
+            punto = new Point(0, 0);
+            tamaño = new Size(40, 40);
+            matriz = new int[15, 15];
+            btnResolver.Enabled = false;
+            btnVerificar.Enabled = false;
+
+        }
+
+        private int[,] matriz;
+        private TextBox[,] matriz_textbox;
+        private int n;
+        private Point punto;
+        private Font fuente;
+        private TextBox txt;
+        private Size tamaño;
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            btnResolver.Enabled = true;
+            tmrLlenar.Enabled = false; 
+            btnVerificar.Enabled = true;
+            if (Convert.ToInt32(txtN.Text) > 0 && Convert.ToInt32(txtN.Text) <= 15 && Convert.ToInt32(txtN.Text) % 2 != 0)
+            {
+                Eliminar(n);
+                CrearTextBox();
+            }
+            else
+            {
+                if (Convert.ToInt32(txtN.Text) > 15 || Convert.ToInt32(txtN.Text) <= 0)
+                {
+                    MessageBox.Show("El tamaño minimo 1 y máximo 15");
+                }
+                if (Convert.ToInt32(txtN.Text) % 2 == 0)
+                {
+                    MessageBox.Show("Debe colocar un número impar");
+                }
+
+            }
+
+        }
+        /// <summary>
+        /// Crear TextBox en tiempo de ejecucion
+        /// </summary>
+        private void CrearTextBox()
+        {
+            n = Convert.ToInt32(txtN.Text);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    txt = new TextBox();
+                    txt.Size = tamaño;
+                    txt.Font = fuente;
+                    txt.Location = punto;
+                    this.matriz_textbox[i, j] = txt;
+                    this.matriz_textbox[i, j].Name = "txt" + (i + 1) + (j + 1);
+                    this.panel1.Controls.Add(matriz_textbox[i, j]);
+                    punto.X += 45;
+                }
+                punto.X = 0;
+                punto.Y += 30;
+            }
+            punto.Y = 0;
+
+        }
+
+        /// <summary>
+        /// Elimana todos los TextBox creados en tiempo de ejecución
+        /// </summary>
+        /// <param name="n"></param>
+        private void Eliminar(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    panel1.Controls.Remove(matriz_textbox[i, j]);
+
+                }
+
+            }
+            GC.Collect();
+        }
+
+
+        /// <summary>
+        /// Boton resolver el cuadrado mediante el algoritmo insertado en el timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = 0;
+                    matriz_textbox[i, j].Text = "";
+                }
+            }
+
+            paso = false;
+            if (trkbrIntervalo.Value == 10)
+            {
+                tmrLlenar.Interval = 10;
+            }
+
+            if (trkbrIntervalo.Value == 9)
+            {
+                tmrLlenar.Interval = 100;
+            }
+
+            if (trkbrIntervalo.Value == 8)
+            {
+                tmrLlenar.Interval = 250;
+            }
+
+            if (trkbrIntervalo.Value == 7)
+            {
+                tmrLlenar.Interval = 500;
+            }
+
+            if (trkbrIntervalo.Value == 6)
+            {
+                tmrLlenar.Interval = 600;
+            }
+
+            if (trkbrIntervalo.Value == 5)
+            {
+                tmrLlenar.Interval = 750;
+            }
+
+            if (trkbrIntervalo.Value == 4)
+            {
+                tmrLlenar.Interval = 850;
+            }
+
+            if (trkbrIntervalo.Value == 3)
+            {
+                tmrLlenar.Interval = 1000;
+            }
+
+            if (trkbrIntervalo.Value == 2)
+            {
+                tmrLlenar.Interval = 1500;
+            }
+
+            if (trkbrIntervalo.Value == 1)
+            {
+                tmrLlenar.Interval = 2000;
+            }
+
+            tmrLlenar.Enabled = true;
+        }
+
+
+        private int numero;
+        private bool paso;
+        private int InicioFila;
+        private int InicioColumna;
+
+        /// <summary>
+        /// Timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tmrLlenar_Tick(object sender, EventArgs e)
+        {
+            btnResolver.Enabled = false;
+            btnVerificar.Enabled = false;
+            if (!paso)
+            {
+                InicioColumna = n / 2;
+                InicioFila = 0;
+                numero = 1;
+                paso = true;
+            }
+
+            if (matriz[InicioFila, InicioColumna] != 0)
+            {
+                InicioFila += 2;
+                InicioColumna--;
+                if (InicioFila == n)
+                {
+                    InicioFila = 0;
+                }
+                if (InicioFila == n + 1)
+                {
+                    InicioFila = 1;
+                }
+                if (InicioColumna == -1)
+                {
+                    InicioColumna = n - 1;
+                }
+            }
+
+            matriz[InicioFila, InicioColumna] = numero++;
+            matriz_textbox[InicioFila, InicioColumna].Text = "" + matriz[InicioFila, InicioColumna];
+
+            InicioFila--;
+            if (InicioFila == -1)
+            {
+                InicioFila = n - 1;
+            }
+            if (InicioColumna == n - 1)
+            {
+                InicioColumna = -1;
+            }
+            InicioColumna++;
+
+
+            if (numero == (n * n) + 1)
+            {
+                btnResolver.Enabled = true;
+                btnVerificar.Enabled = true;
+                tmrLlenar.Enabled = false;
+                paso = false;
+            }
+
+        }
+
+        /// <summary>
+        /// Sumatoria para verificar las filas, columnas, diagnales;
+        /// </summary>
+        /// <returns></returns>
+        private int Sumatoria()
+        {
+            return ((n * n) * ((n * n) + 1)) / (2 * n);
+
+        }
+
+        /// <summary>
+        /// Verificar si el cuadrado mágico está correcto
+        /// </summary>
+        /// <returns></returns>
+        private Boolean VerificarCuadrado()
+        {
+            int sumatoria = Sumatoria();
+            int suma = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz_textbox[i, j].Text.Length != 0)
+                    {
+                        suma += Convert.ToInt32(matriz_textbox[i, j].Text);
+                    }
+                    else return false;
+
+                }
+                if (suma != sumatoria)
+                {
+                    return false;
+                }
+                suma = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz_textbox[i, j].Text.Length != 0)
+                    {
+                        suma += Convert.ToInt32(matriz_textbox[i, j].Text);
+                    }else
+                    return false;
+                }
+                if (suma != sumatoria)
+                {
+                    return false;
+                }
+
+                suma = 0;
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                if ((suma != sumatoria && i == n) || matriz_textbox[i, i].Text.Length == 0)
+                {
+                    return false;
+                }
+
+                suma += Convert.ToInt32(matriz_textbox[i, i].Text);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Boton verificar, muestra un Cuadro de Dialogo dependiendo del metodo VerificarCuadrado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            if (VerificarCuadrado())
+            {
+                MessageBox.Show("Buen trabajo");
+            }else MessageBox.Show("Lo siento, siga intentando");
+        }
+    }
+}
